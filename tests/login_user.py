1409a18c84b0e0be8f7d502aa92fa7954a52e819
Soegidev <virtualsoegi88@gmail.com>
2025-04-19 12:55:12 +0700

    jkjk

---
+++ tests/login_user.py
@@ -0,0 +1,21 @@
+"""System module."""
+import json
+import requests
+BASE_URL = "http://127.0.0.1:8080/"
+def login() :
+    """Doc String"""
+    path = "login"
+    mimetype = 'application/json'
+    username = 'mistersoegi'
+    password= 'pass12345!'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+    }
+    data = {
+        'username': username,
+        'password': password
+    }
+    response = requests.post(BASE_URL+path,data=json.dumps(data), headers=headers)
+    assert response.status_code == 200
+    
